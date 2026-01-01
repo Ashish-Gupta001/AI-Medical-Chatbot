2377b3c1d6c3a623412052e40938f596ef0b14c4
Ashish Gupta <ashishgupta.ag999@gmail.com>
2026-01-01 22:00:50 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#  Making  Directories
+mkdir -p src
+mkdir -p research
+
+
+#  Making files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.py
+
+echo "Directory and files created successfully!"
